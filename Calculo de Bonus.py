7795216cc6ec9beaa26243507a790a3bb1c9c3c0
VNCprojetos <149001791+VNCprojetos@users.noreply.github.com>
2023-12-14 19:21:36 -0300

    Update Calculo de Bonus.py

--- Calculo de Bonus.py
+++ Calculo de Bonus.py
@@ -1,18 +1,18 @@
 meta_funcionario = 10000
 meta_loja = 250000
 vendas_funcionario = 15000
-vendas_loja = 200000
-nota = 10
-
-# A NOTA SENDO MAIOR QUE 9 TAMBÉM GERA BONUS, não é acumulativo com o bonus de meta atingida.
-# SE O FUNCIONARIO ALCANÇOU A META ESTIPULADA NÃO É NECESSÁRIO DA NOTA!
+vendas_loja = 260000
+nota = 9
 
 if vendas_funcionario > meta_funcionario and vendas_loja > meta_loja:
     bonus = 0.03 * vendas_funcionario
     print('Ganhou Bonus por meta alcançada, Bonus do funcionario foi de {}'.format(bonus))
+    print('Recebeu a Nota: {}'.format(nota))
+
 elif nota >= 9 or (vendas_funcionario > meta_funcionario):
     bonus2 = 0.01 * vendas_funcionario
     print('Funcionario Ganhou bonus por Nota ou Meta Individual, Bonus de {}'.format(bonus2))
+    print('Recebeu a Nota: {}'.format(nota))
 else:
     print('Funcionario não tem direito ao bonus')
 
